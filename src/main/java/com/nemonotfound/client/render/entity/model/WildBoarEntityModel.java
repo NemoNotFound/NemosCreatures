109d19266cde4969f1ad6569dc57ead71d33a2b5
NemoNotFound <unconfigured@null.spigotmc.org>
2024-04-18 22:12:33 +0400

    Updated the wild boar texture
    (cherry picked from commit 2244e3c2447b35400cde5e9a0ec2c10d334d689e)

--- src/main/java/com/nemonotfound/client/render/entity/model/WildBoarEntityModel.java
+++ src/main/java/com/nemonotfound/client/render/entity/model/WildBoarEntityModel.java
@@ -21,19 +21,35 @@ public class WildBoarEntityModel<T extends WildBoarEntity> extends SinglePartEnt
     public static TexturedModelData getTexturedModelData() {
         ModelData modelData = new ModelData();
         ModelPartData modelPartData = modelData.getRoot();
-        ModelPartData wildBoarModelPartData = modelPartData.addChild("wild_boar", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 8.0F, -7.0F));
-        wildBoarModelPartData.addChild("body", ModelPartBuilder.create().uv(0, 0).cuboid(-5.0F, 2.0F, -2.0F, 10.0F, 8.0F, 16.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
-        ModelPartData head = wildBoarModelPartData.addChild("head", ModelPartBuilder.create().uv(0, 24).cuboid(-4.0F, 0.0F, -8.0F, 8.0F, 8.0F, 8.0F, new Dilation(0.0F))
-                .uv(36, 10).cuboid(-3.0F, 4.0F, -11.0F, 6.0F, 3.0F, 3.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
-        head.addChild("horn_right", ModelPartBuilder.create().uv(0, 10).cuboid(-5.0F, -11.0F, -17.0F, 2.0F, 1.0F, 1.0F, new Dilation(0.0F))
-                .uv(0, 0).cuboid(-5.0F, -13.0F, -17.0F, 1.0F, 2.0F, 1.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 16.0F, 7.0F));
-        head.addChild("horn_left", ModelPartBuilder.create().uv(6, 10).cuboid(3.0F, -11.0F, -17.0F, 2.0F, 1.0F, 1.0F, new Dilation(0.0F))
-                .uv(11, 11).cuboid(4.0F, -13.0F, -17.0F, 1.0F, 2.0F, 1.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 16.0F, 7.0F));
-        ModelPartData legs = wildBoarModelPartData.addChild("legs", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 16.0F, 7.0F));
+        ModelPartData wild_boar = modelPartData.addChild("wild_boar", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 24.0F, 0.0F));
+
+        ModelPartData body = wild_boar.addChild("body", ModelPartBuilder.create().uv(0, 0).cuboid(-5.0F, -14.0F, -9.0F, 10.0F, 8.0F, 16.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+
+        ModelPartData back = body.addChild("back", ModelPartBuilder.create(), ModelTransform.of(1.0F, 0.0F, 0.0F, 0.0F, 0.0F, -0.0873F));
+
+        back.addChild("cube_r1", ModelPartBuilder.create().uv(20, 28).cuboid(-2.6889F, -15.0301F, -6.0F, 3.0F, 1.0F, 12.0F, new Dilation(0.0F))
+                .uv(0, 10).cuboid(-1.6889F, -16.0301F, -6.0F, 1.0F, 1.0F, 5.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0873F));
+
+        ModelPartData head = wild_boar.addChild("head", ModelPartBuilder.create().uv(0, 24).cuboid(-4.0F, -4.0F, -7.0F, 8.0F, 8.0F, 8.0F, new Dilation(0.0F))
+                .uv(38, 34).cuboid(-3.0F, 0.0F, -10.0F, 6.0F, 3.0F, 3.0F, new Dilation(0.0F))
+                .uv(36, 0).cuboid(-1.6889F, -5.0301F, -8.0F, 3.0F, 3.0F, 10.0F, new Dilation(0.0F))
+                .uv(32, 41).cuboid(-0.6889F, -6.0301F, -7.0F, 1.0F, 1.0F, 5.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -12.0F, -8.0F));
+
+        head.addChild("horn_right", ModelPartBuilder.create().uv(7, 10).cuboid(-5.0F, -11.0F, -17.0F, 2.0F, 1.0F, 1.0F, new Dilation(0.0F))
+                .uv(0, 0).cuboid(-5.0F, -13.0F, -17.0F, 1.0F, 2.0F, 1.0F, new Dilation(0.0F)), ModelTransform.of(1.0F, 12.0F, 8.0F, 0.0F, 0.0F, -0.0873F));
+
+        head.addChild("horn_left", ModelPartBuilder.create().uv(7, 12).cuboid(3.0F, -11.0F, -17.0F, 2.0F, 1.0F, 1.0F, new Dilation(0.0F))
+                .uv(0, 10).cuboid(4.0F, -13.0F, -17.0F, 1.0F, 2.0F, 1.0F, new Dilation(0.0F)), ModelTransform.of(-1.0F, 12.0F, 8.0F, 0.0F, 0.0F, 0.0873F));
+
+        ModelPartData legs = wild_boar.addChild("legs", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+
         legs.addChild("back_right", ModelPartBuilder.create().uv(0, 0).cuboid(-2.0F, 0.0F, -2.0F, 4.0F, 6.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(-3.0F, -6.0F, 6.0F));
-        legs.addChild("front_right", ModelPartBuilder.create().uv(32, 24).cuboid(-2.0F, 0.0F, -2.0F, 4.0F, 6.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(-3.0F, -6.0F, -6.0F));
-        legs.addChild("back_left", ModelPartBuilder.create().uv(36, 0).cuboid(-2.0F, 0.0F, -2.0F, 4.0F, 6.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(3.0F, -6.0F, 6.0F));
-        legs.addChild("front_left", ModelPartBuilder.create().uv(32, 34).cuboid(-2.0F, 0.0F, -2.0F, 4.0F, 6.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(3.0F, -6.0F, -6.0F));
+
+        legs.addChild("back_left", ModelPartBuilder.create().uv(16, 41).cuboid(-2.0F, 0.0F, -2.0F, 4.0F, 6.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(3.0F, -6.0F, 6.0F));
+
+        legs.addChild("front_left", ModelPartBuilder.create().uv(0, 40).cuboid(-2.0F, 0.0F, -2.0F, 4.0F, 6.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(3.0F, -6.0F, -6.0F));
+
+        legs.addChild("front_right", ModelPartBuilder.create().uv(38, 24).cuboid(-2.0F, 0.0F, -2.0F, 4.0F, 6.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(-3.0F, -6.0F, -6.0F));
 
         return TexturedModelData.of(modelData, 64, 64);
     }
